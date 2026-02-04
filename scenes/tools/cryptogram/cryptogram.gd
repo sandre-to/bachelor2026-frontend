a0fe3dd354e7fa76f92964bad93a362a0bc70f60
sandre-to <sandre.mnto@gmail.com>
2026-02-04 16:15:56 +0100

    Testet med hash table

--- scenes/tools/cryptogram/cryptogram.gd
+++ scenes/tools/cryptogram/cryptogram.gd
@@ -1,8 +1,10 @@
 class_name CryptogramPuzzle extends Control
 
+#Engelsk alfabetet
 const ALPHABET_LETTERS: String = "abcdefghijklmnopqrstuvwxyz"
 
-var test_string: String = "good good"
+var test_string: String = "abc"
+var mapped_letters: Dictionary[String, String] = {}
 
 func _ready() -> void:
 	_randomize_alphabet()
@@ -11,20 +13,32 @@ func _ready() -> void:
 func _substitute_letter(input_word: String) -> void:
 	var scrambled_word: Array[String]
 	var new_alphabet := _randomize_alphabet()
+	var index = 0
 	
-	for i in input_word.length():
-		for j in ALPHABET_LETTERS.length():
-			if input_word[i] == " ":
-				scrambled_word.append(" ")
-				break
-			
-			if input_word[i] == ALPHABET_LETTERS[j]:
-				scrambled_word.append(new_alphabet[j])
+	while index < ALPHABET_LETTERS.length():
+		mapped_letters[ALPHABET_LETTERS[index]] = new_alphabet[index]
+		index += 1
+		
+	for i in mapped_letters.size():
+		print("Keys: %s Value: %s" % [mapped_letters.keys()[i], mapped_letters.values()[i]])
+		
+	#for i in input_word.length():
+		#for j in ALPHABET_LETTERS.length():
+			##Sjekker om det er mellomrom i ordet. 
+			##Hvis det er, så hopper den over til neste iterasjon.
+			#if input_word[i] == " ":
+				#scrambled_word.append(" ")
+				#break
+			#
+			##Legger til bokstavene i en ny array
+			#if input_word[i] == ALPHABET_LETTERS[j]:
+				#scrambled_word.append(new_alphabet[j])
 	
-	print(ALPHABET_LETTERS)
-	print(new_alphabet)
-	print(scrambled_word)
-	
+	#Print meldinger for debugging
+	#print(new_alphabet)
+	#print(scrambled_word)
+
+# Funksjon for å endre rekkefølgen på alfabetet
 func _randomize_alphabet() -> Array[String]:
 	var new_alphabet: Array[String]
 	for i in range(ALPHABET_LETTERS.length()):
