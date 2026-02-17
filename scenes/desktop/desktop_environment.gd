72733014212d05321ce4d27c94aecc69e32623af
sandre <sandre.mnto@gmail.com>
2026-02-17 13:08:36 +0100

    krypto verktøy klart

--- scenes/desktop/desktop_environment.gd
+++ scenes/desktop/desktop_environment.gd
@@ -1,8 +1,21 @@
 class_name Desktop extends Control
 
 const WINDOW: PackedScene = preload("res://scenes/window/window.tscn")
+var root: Node = null
+
+func _ready() -> void:
+	root = get_tree().root
 
 func _on_crypto_button_pressed() -> void:
-	var window := WINDOW.instantiate()
-	window.current_scene = SceneManager.find_tool_scene(Tool.ToolType.CRYPTOTOOL)
-	get_tree().current_scene.add_child(window)
+	if not _tool_exists(root, Tool.ToolType.CRYPTOTOOL):
+		var window := WINDOW.instantiate()
+		window.current_scene = SceneManager.find_tool_scene(Tool.ToolType.CRYPTOTOOL)
+		root.add_child(window)
+
+func _tool_exists(root: Node, type: Tool.ToolType) -> bool:
+	for node in root.get_children():
+		if node is CustomWindow:
+			var child_node = node.get_child(0)
+			if child_node.tool_type == type:
+				return true
+	return false
