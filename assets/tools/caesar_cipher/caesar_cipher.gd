e806e9ab2416b01af1d08590de56a1e7a7353304
sandre-to <sandre.mnto@gmail.com>
2026-02-18 23:03:49 +0100

    testet med statisk krypto oppgave

--- assets/tools/caesar_cipher/caesar_cipher.gd
+++ assets/tools/caesar_cipher/caesar_cipher.gd
@@ -6,8 +6,6 @@ class_name CaesarCipher extends Tool
 @onready var shift_input: LineEdit = $MainPanel/LineEdit
 @onready var output_label: RichTextLabel = $OutputPanel/OutputLabel
 
-var encryption: bool = false
-
 func encrypt(text: String, shift: int) -> String:
 	if text_input.text == "":
 		push_error("User entered empty text.")
@@ -17,38 +15,34 @@ func encrypt(text: String, shift: int) -> String:
 		push_error("Shift entered is not a valid number.")
 		return ""
 	
-	if encryption:
-		if shift > 9 or shift < 0: 
-			push_error("The number entered is bigger than 9.")
-			return ""
-	else:
-		if -shift > 9 or -shift < 0: 
-			push_error("The number entered is bigger than 9.")
-			return ""
+	if abs(shift) > 9:
+		push_error("The number entered is bigger than 9.")
+		return ""
 	
 	var result := ""
 	# Gå gjennom hele teksten (input fra bruker)
 	for i in range(text.length()):
-		var character := text[i]
+		var c := text[i]
+		var code := c.unicode_at(0)
 		
 		# Hvis det er store bokstaver (blokk bokstaver)
-		if character >= "A" and character <= "Z":
-			result += char((character.unicode_at(0) + shift - 65) % 26 + 65)
-		# Hvis det er små bokstaver
-		elif character >= "a" and character <= "z":
-			result += char((character.unicode_at(0) + shift - 97) % 26 + 97)
+		if code >= 65 and code <= 90:
+			result += char(mod(code + shift - 65, 26) + 65)
+		elif code >= 97 and code <= 122:
+			result += char(mod(code + shift - 97, 26) + 97)
 		else:
-			result += character
-	
+			result += c
+		
 	return result
 
 func valid_shift(input: String) -> bool:
 	return input.is_valid_int()
 
 func _on_encrypt_pressed() -> void:
-	encryption = true
 	output_label.text = encrypt(text_input.text, int(shift_input.text))
 
 func _on_decrypt_pressed() -> void:
-	encryption = false
 	output_label.text = encrypt(text_input.text, -int(shift_input.text))
+
+func mod(a: int, b: int) -> int:
+	return (a % b + b) % b
--- scenes/window/window.gd
+++ scenes/window/window.gd
@@ -3,7 +3,8 @@ class_name CustomWindow extends Window
 var current_scene: PackedScene = null
 
 func _ready() -> void:
-	add_child(current_scene.instantiate())
+	if current_scene:
+		add_child(current_scene.instantiate())
 
 func _on_close_requested() -> void:
 	queue_free()
