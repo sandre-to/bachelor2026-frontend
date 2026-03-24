ea4763ac10e54dd2491d468fc46db43b02e130e9
Folkvord <folkvord.kristoffer@gmail.com>
2026-03-24 06:40:30 -0400

    lol

--- scenes/network_test.gd
+++ scenes/network_test.gd
@@ -11,61 +11,23 @@ var task: Task = Task.new(SPNetwork.new())
 func _ready() -> void:
 	NetworkManager.connect_to_backend()
 	NetworkManager.message_received.connect(_on_message)
-
-
-func _process(delta: float) -> void:
-	
-	if not task_requested:
-		NetworkManager.send("task", {"taskID": 0})
-		task_requested = true
-		
-	elif task_ready:
-		task.start()
-		task_ready = false
-	
-	
-	
-	
-	
-func task_msg_receivced(status: String, data: Dictionary):
-	if status == "error":
-		print(data)
-		push_error("task_msg_receivced(): status")
-		return
-	
-	task = task_parser.parse(
-		JSON.stringify(data), task
-	)
-	
-	if task_parser.get_error() != 0:
-		NetworkManager.send_own({
-			"type": "parse-status",
-			"status": "error",
-			"data": {
-				"desc": task_parser.get_error_desc()
-			}
-		})
-		push_error("task_msg_receivced(): parser")
-		return
 	
 	NetworkManager.send_own({
-		"type": "parse-status",
-		"status": "success"
+		"type": "task",
+		"data": {
+			"taskID": 67
+		}
 	})
 	
 	
-	
-	
+
 func _on_message(msg: Dictionary) -> void:
 	var type 	= msg.get("type")
 	var status 	= msg.get("status")
 	var data 	= msg.get("data")
 	
-	if type == "task":
-		task_msg_receivced(status, data)
-	elif type == "parse-status":
-		task_ready = true
-		
-	
-	
-	pass
+	print()
+	print(type)
+	print(status)
+	print(data)
+	print()
